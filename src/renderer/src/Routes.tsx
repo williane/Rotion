78c78e72e47ef8330cc8685ec8af761d8aca4b20
Williane <williane.tads@gmail.com>
2022-12-27 11:10:15 -0300

    configurando layout

--- src/renderer/src/Routes.tsx
+++ src/renderer/src/Routes.tsx
@@ -2,20 +2,17 @@ import { Router, Route } from "electron-router-dom";
 
 import { Blank } from "./pages/blank";
 import { Document } from "./pages/document";
-// import { Default } from "./pages/layouts/default";
+import { Default } from "./pages/layouts/default";
 
 export function Routes() {
   return (
     <Router
       main={
-        // <Route path="/" element={<Default />}>
-        //   <Route path="/" element={<Blank />} />
-        //   <Route path="/documents/:id" element={<Document />} />
-        // </Route>
-        <>
+        <Route path="/" element={<Default />}>
           <Route path="/" element={<Blank />} />
           <Route path="/document" element={<Document />} />
-        </>
+          {/* <Route path="/documents/:id" element={<Document />} /> */}
+        </Route>
       }
     />
   );
