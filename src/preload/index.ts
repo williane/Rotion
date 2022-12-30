5569e05bf448ade0aa4a81cbd0f8d8236ef98fe7
Williane <williane.tads@gmail.com>
2022-12-30 08:42:07 -0300

    Exemplo comunicação IPC

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,22 +1,30 @@
-import { contextBridge } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
+import { contextBridge, ipcRenderer } from "electron";
+import { ElectronAPI, electronAPI } from "@electron-toolkit/preload";
 
 // Custom APIs for renderer
-const api = {}
+declare global {
+  export interface Window {
+    electron: ElectronAPI;
+    api: typeof api;
+  }
+}
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
+const api = {
+  fetchDocuments(params: any) {
+    return ipcRenderer.invoke("fetch-documents", params);
+  },
+};
+
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
+    contextBridge.exposeInMainWorld("electron", electronAPI);
+    contextBridge.exposeInMainWorld("api", api);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 } else {
   // @ts-ignore (define in dts)
-  window.electron = electronAPI
+  window.electron = electronAPI;
   // @ts-ignore (define in dts)
-  window.api = api
+  window.api = api;
 }
